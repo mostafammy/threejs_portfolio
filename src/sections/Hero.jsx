6772d6addd3d52cd6c5fb55b024557780b4ff2be
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-05-10 17:06:58 +0300

    Optimizing HackerRoom 3D Element

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
-import {PerspectiveCamera} from "@react-three/drei";
-import {Canvas} from "@react-three/fiber";
+import {PerformanceMonitor, PerspectiveCamera} from "@react-three/drei";
+import round from 'lodash/round'
+import {Canvas, useThree} from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom.jsx";
-import {Suspense} from "react";
+import {Suspense, useEffect, useState} from "react";
 import CanvasLoader from "../components/CanvasLoader.jsx";
 // import {Leva, useControls} from "leva";
 import {useMediaQuery} from "react-responsive";
@@ -13,6 +14,7 @@ import Cube from "../components/Cube.jsx";
 import Rings from "../components/Rings.jsx";
 import HeroCamera from "../components/HeroCamera.jsx";
 import Button from "../components/Button.jsx";
+
 
 const Hero = () => {
     // const x = useControls('ReactLogo', {
@@ -53,11 +55,27 @@ const Hero = () => {
     //             max: 10,
     //     }
     // });
+
+
     const isSmall = useMediaQuery({maxWidth: 440});
     const isMobile = useMediaQuery({maxWidth: 768});
     const isTablet = useMediaQuery({minWidth: 768, cmaxWidth: 1024});
 
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+
+    const [dpr, setDpr] = useState(1.5);
+
+    function AdaptivePixelRatio() {
+        const current = useThree((state) => state.performance.current)
+        const setPixelRatio = useThree((state) => state.setDpr)
+        useEffect(() => {
+            setPixelRatio(window.devicePixelRatio * current)
+        }, [current])
+        return null
+    }
+    
+
     return (
         <section className={"min-h-screen border-blue-500 w-full flex relative flex-col"}>
             <div className={"w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3"}>
@@ -67,27 +85,31 @@ const Hero = () => {
             </div>
             <div className={"w-full h-full absolute inset-0 mt-12"}>
                 {/*<Leva/>*/}
-                <Canvas className={"w-full h-full"}>
-                    <Suspense fallback={<CanvasLoader/>}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 20]}/>
-                        <HeroCamera isMobile={isMobile}>
-                            <HackerRoom
-                                /*scale={0.07}*/
-                                scale={sizes.deskScale}
-                                position={sizes.deskPosition}
-                                rotation={[0, -Math.PI, 0]}
-                            />
-                        </HeroCamera>
-                        <group>
-                            <Target position={sizes.targetPosition}/>
-                            <ReactLogo position={sizes.reactLogoPosition}/>
-                            {/*<ReactLogo position={[x.positionX, x.positionY, x.positionZ]} />*/}
-                            <Cube position={sizes.cubePosition}/>
-                            <Rings position={sizes.ringPosition}/>
-                        </group>
-                        <ambientLight intensity={1}/>
-                        <directionalLight intensity={0.5} position={[10, 10, 10]}/>
-                    </Suspense>
+                <Canvas frameloop={isMobile ? 'demand' : 'always'} className={"w-full h-full"} dpr={dpr}>
+                    <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(1)}
+                                        onChange={({factor}) => setDpr(round(0.5 + 1.5 * factor, 1))}>
+                        <Suspense fallback={<CanvasLoader/>}>
+                            <PerspectiveCamera makeDefault position={[0, 0, 20]}/>
+                            <AdaptivePixelRatio pixelRatio={Math.min(window.devicePixelRatio, 2)}/>
+                            <HeroCamera isMobile={isMobile}>
+                                <HackerRoom
+                                    /*scale={0.07}*/
+                                    scale={sizes.deskScale}
+                                    position={sizes.deskPosition}
+                                    rotation={[0, -Math.PI, 0]}
+                                />
+                            </HeroCamera>
+                            <group>
+                                <Target position={sizes.targetPosition}/>
+                                <ReactLogo position={sizes.reactLogoPosition}/>
+                                {/*<ReactLogo position={[x.positionX, x.positionY, x.positionZ]} />*/}
+                                <Cube position={sizes.cubePosition}/>
+                                <Rings position={sizes.ringPosition}/>
+                            </group>
+                            <ambientLight intensity={1}/>
+                            <directionalLight intensity={0.5} position={[10, 10, 10]}/>
+                        </Suspense>
+                    </PerformanceMonitor>
                 </Canvas>
             </div>
 
